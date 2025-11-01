cd718b86d673500cbae19f6a3ea9ad7e6aa71497
Sadik Ahsan <sadikahasan6@gmail.com>
2025-11-01 19:27:39 +0600

    init

--- src/routes/jobs/+page.svelte
+++ src/routes/jobs/+page.svelte
@@ -1,1 +1,34 @@
-<div class="text-2xl"> Available Jobs (54)</div>
+<div class="text-sm flex gap-8 mb-3 text-gray-800 dark:text-zinc-200">
+    <div class="cursor-pointer">Production</div>
+    <div class="cursor-pointer">Planning</div>
+    <div class="cursor-pointer">Human Resources</div>
+    <div class="cursor-pointer">Finance & Accounting</div>
+    <div class="cursor-pointer">Health & Safety</div>
+    <div class="cursor-pointer">Development & IT</div>
+    <div class="cursor-pointer">More...</div>
+</div>
+<div class=" py-10">
+    <div class="text-4xl text-center">Find the best jobs</div>
+    <div class="text-lg text-center mt-4">
+        Explore job opportunities and get all the info you need.
+    </div>
+    <div class="text-center text-zinc-700 dark:text-zinc-400 mt-3">
+        50+ Organizations collect empoyees from here
+    </div>
+    <div class="mx-auto w-fit mt-6">
+        <a
+            href="/jobs"
+            class="w-fit cursor-pointer px-4 py-2 rounded-lg dark:bg-white/70 bg-black/70 text-white dark:text-black"
+            >Apply now</a
+        >
+    </div>
+</div>
+
+<div class="flex py-10 gap-6">
+    <div class="border border-blue-100 rounded-lg p-6 w-1/2">
+        HEllo
+    </div>
+        <div class="border border-blue-100 rounded-lg p-6 w-1/2">
+        HEllo
+    </div>
+</div>
