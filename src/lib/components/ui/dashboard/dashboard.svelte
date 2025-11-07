75b763ac8f88129854a5e3a4e87a2484ad45a7f6
Sadik Ahsan <sadikahasan6@gmail.com>
2025-11-07 20:13:50 +0600

    init

--- src/lib/components/ui/dashboard/dashboard.svelte
+++ src/lib/components/ui/dashboard/dashboard.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
     import SideNav from "$lib/components/ui/dashboard/side-nav.svelte";
     import TopNav from "$lib/components/ui/dashboard/top-nav.svelte";
+    import { onMount } from "svelte";
     export let navItems: { name: string; href: string }[] = [];
-    
+
     // Add state for sidebar visibility
     let isSidebarOpen = true;
+
+    const checkScreenSize = () => {
+        // Tailwind's md breakpoint is 768px
+        if (window.matchMedia("(max-width: 1024px)").matches) {
+            isSidebarOpen = false;
+        } else {
+            isSidebarOpen = true;
+        }
+    };
+
+    // Run once on mount
+    onMount(() => {
+        checkScreenSize();
+        // Update if window resizes
+        window.addEventListener("resize", checkScreenSize);
+        return () => window.removeEventListener("resize", checkScreenSize);
+    });
 
     // Toggle function
     const toggleSidebar = () => {
@@ -12,18 +30,28 @@
     };
 </script>
 
-<div class="h-screen flex flex-col gap-2 fixed inset-0 w-full bg-linear-to-r dark:text-zinc-300 from-white to-blue-50/50 dark:from-zinc-700 dark:to-gray-800 overflow-hidden">
+<div
+    class="h-screen flex flex-col gap-2 fixed inset-0 w-full bg-linear-to-r dark:text-zinc-300 from-white to-blue-50/50 dark:from-zinc-700 dark:to-gray-800 overflow-hidden"
+>
     <TopNav {toggleSidebar} />
 
     <div class="flex px-7 flex-1 gap-1 overflow-hidden">
         <!-- Add transition classes for smooth animation -->
-        <div class="transition-all duration-300 ease-in-out {isSidebarOpen ? 'w-68' : 'w-0'} overflow-hidden">
+        <div
+            class="transition-all duration-300 ease-in-out {isSidebarOpen
+                ? 'w-68 max-md:full'
+                : 'w-0'} overflow-hidden"
+        >
             <div class="w-68 flex flex-col pr-5 gap-2">
                 <SideNav {navItems} />
             </div>
         </div>
 
-        <div class="flex-1 overflow-y-auto bg-white/80 rounded-2xl border border-blue-50 dark:border-0 dark:bg-zinc-600/85 dark:text-white px-10 py-5 mb-5">
+        <div
+            class="flex-1 overflow-y-auto bg-white/80 rounded-2xl border {isSidebarOpen
+                ? 'max-lg:hidden'
+                : ''} border-blue-50 dark:border-0 dark:bg-zinc-600/85 dark:text-white px-10 py-5 mb-5"
+        >
             <slot />
         </div>
     </div>
